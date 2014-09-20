caff0b3d1bbedd9a06a59bf5b62fdff0f56b0a18
kyfho <kyfho@users.noreply.github.com>
2014-09-20 14:32:23 -0700

    Update linkenize.bash

--- linkenize.bash
+++ linkenize.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-# site file has url linkname as many as you want
-while read -r name url; do echo "<a href='$url'>$name</a>"; done < sites
+# sites.txt file has linkname URL, one per line, as many as you want
+while read -r name url; do echo "<a href='$url'>$name</a>"; done < sites.txt
